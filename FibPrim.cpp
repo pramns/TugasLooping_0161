d5f9f15e55661b3e5e55e03b90857f7cdeddf929
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-03-31 08:07:54 +0700

    Menambah Void menu

--- FibPrim.cpp
+++ FibPrim.cpp
@@ -4,3 +4,12 @@ using namespace std;
 int angka;
 int pilihan;
 
+void Menu() {
+    cout << "\n=== MENU PENGECEKAN BILANGAN ===" << endl;
+    cout << "1. Cek Bilangan Prima" << endl;
+    cout << "2. Cek Bilangan Fibonacci" << endl;
+    cout << "0. Keluar" << endl;
+    cout << "Masukkan pilihan Anda (0-2): ";
+}
+
+
